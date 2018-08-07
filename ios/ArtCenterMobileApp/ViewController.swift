ed48fc5a4c33abc787b2ec54f97cc702c53a91bc
luxuc <yoocecil@gmail.com>
2018-08-07 16:07:46 +0800

    add ios assignment

---
+++ ios/ArtCenterMobileApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ArtCenterMobileApp
+//
+//  Created by Lu Xu on 2018/7/22.
+//  Copyright © 2018 Lu Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
